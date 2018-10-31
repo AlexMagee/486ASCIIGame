78233a4a75b40331d929e038e27caf6ff139daac
Alex Magee <Alex.Magee1997@GMail.com>
2018-10-31 16:40:04 -0500

    Android Attempt

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ currency = 0
 mapOffset = {15,15}
 
 function love.load()
+  width, height = love.graphics.getDimensions()
   tile = {}
 	for i=0,10 do
 		tile[i] = love.graphics.newImage( "assets/tile"..i..".png" )
@@ -26,6 +27,14 @@ function love.load()
 	map_x = -mapOffset[1] - tile_w/2
 	map_y = -mapOffset[2] - tile_h/2
 	map_display_buffer = 2
+  print(height)
+  print(tile_h*map_h)
+  print(height/(tile_h*map_h))
+end
+ 
+function love.touchmoved( id, x, y, dx, dy, pressure )
+  print(dx)
+  print(dy)
 end
  
 function love.keypressed(key, scancode, isrepeat)
@@ -33,6 +42,7 @@ function love.keypressed(key, scancode, isrepeat)
 end
  
 function love.draw()
+  love.graphics.scale(height/(tile_h*map_h+mapOffset[1]*2),height/(tile_h*map_h+mapOffset[2]*2))
 	drawMap(realmap.landscape.tiles, realmap.landscape.colors)
   drawMap(realmap.entities.tiles, realmap.entities.colors)
   love.graphics.setColor(255,255,255)
